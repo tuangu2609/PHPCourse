7d40cac01ee6877eb4dec3b7a5c651f4b278bcc9
Tuan Nguyen <tuannguyenminh5d@gmail.com>
2023-08-06 16:15:44 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="todo_name" placeholder="Enter your todo">
+        <button>New Todo</button>
+    </form>
+</body>
+</html>
